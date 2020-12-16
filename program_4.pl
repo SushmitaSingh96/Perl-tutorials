bdf96ec063680fc0dcc627c88b2b99e715e7b213
Sushmita Singh <40317759+SushmitaSingh96@users.noreply.github.com>
2020-12-16 14:06:48 +0530

    Update program_4.pl

--- program_4.pl
+++ program_4.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 sub main{
-	my $file = '/home/sushmita/Documents/chats.txt';
+	my $file = '/home/chats.txt';
 	open (INPUT, $file) or die("Input file $file does not exists\n");
 	while (my $line = <INPUT>){
 		print $line;
