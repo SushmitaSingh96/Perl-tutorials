a671d6eec8639b65e441c6b32a0101539642156a
Sushmita Singh <40317759+SushmitaSingh96@users.noreply.github.com>
2020-12-16 14:15:14 +0530

    Update program_11.pl

--- program_11.pl
+++ program_11.pl
@@ -14,19 +14,16 @@ sub main{
 	
 	print $months{Nov}."\n";
 	
-	# we can use key function to obtain all keys from the hash, likewise there is a values function as well.
 	foreach my $tmp(sort keys %months){
 		my $value = $months{$tmp};
 		print "$value : $tmp\n";
 	}
-	#using 'each' keyword we can assign arrays of keys and values to scalars
 	while (my ($keys, $values) = each %months){
 	
 		print "$keys:$values\n";
 	
 	}
 	push my @arr,\%months;
-	#print Dumper(@arr);
 	print $arr[0].{'Mar'};
 	print Dumper(@arr);
 
