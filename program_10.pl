d0ae4bb53c5fb13572d359413d308dd5ada0b84d
Sushmita Singh <40317759+SushmitaSingh96@users.noreply.github.com>
2020-12-16 14:14:11 +0530

    Update program_10.pl

--- program_10.pl
+++ program_10.pl
@@ -9,16 +9,12 @@ sub main{
 	push @values, \@arr1;
 	push @values, \@arr2;
 	print Dumper(@values)."\n";
-	#here also we need a de reference to access individual values of arrays.
 	foreach my $tmp(@values){
 		print $tmp."\n";
 		print $tmp->[0]."\n";
 	}
-	#notice that we use scaler $ not array @.
 	print $values[0][0]."\n";
-	#if we create only a ref:
 	my $ref=\@arr1;
-	# we have to use a de referencer -> to access the values in that array:
 	print $ref->[1]."\n"; 
 }
 
